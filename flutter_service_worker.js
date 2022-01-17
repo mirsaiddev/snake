37f0c183487639de620adb7ff3f5bdc2b50d8e1f
sinavyoktr <96592517+sinavyoktr@users.noreply.github.com>
2022-01-17 06:41:42 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,6 +3,59 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
+<<<<<<< HEAD
+  "version.json": "6f9506ed96ba6b3ac328b9be9cf7c518",
+"index.html": "06df7e48b36a8f936a02c24e27d33ae1",
+"/": "06df7e48b36a8f936a02c24e27d33ae1",
+"main.dart.js": "eedb26af308e96461a67cd006ee41de6",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "92b981e5821151c62f5e75fdb666ae5f",
+".git/ORIG_HEAD": "e3e4f47e4a40fff2c0ded7fc09060601",
+".git/config": "6623f6fe716304c8b7a0fd3e635f97ea",
+".git/objects/fc/a7f068ac0a780b52aa05541a61b49e9842ac3d": "6c966028fb4c72ad81138837392e6ca4",
+".git/objects/20/5bb5db271c6d8de8399864c7bb9b917f638893": "c993b22f115d7f3ae6d5b7b212806539",
+".git/objects/8a/aa46ac1ae21512746f852a42ba87e4165dfdd1": "1d8820d345e38b30de033aa4b5a23e7b",
+".git/objects/88/cfd48dff1169879ba46840804b412fe02fefd6": "e42aaae6a4cbfbc9f6326f1fa9e3380c",
+".git/objects/37/2c9902101acd53a555abb6d66343a11d4718e6": "e35e7a68a30990b39990256433967a7e",
+".git/objects/0a/3555320965118af77618a31d7e5408eec83110": "ebd501ce3d0b818839013d537a58d808",
+".git/objects/b7/49bfef07473333cf1dd31e9eed89862a5d52aa": "36b4020dca303986cad10924774fb5dc",
+".git/objects/f1/e6741eaa0bd004ab09682708c6bf92d6d81396": "b9a1197b23531d1a20d9ea63f5be4285",
+".git/HEAD": "cf7dd3ce51958c5f13fece957cc417fb",
+".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
+".git/logs/HEAD": "5f720fc169cb9e7fc6c94fe8c2e8e476",
+".git/logs/refs/heads/main": "02e6bc7b7a4e7c9afa77051f4599c1be",
+".git/logs/refs/remotes/origin/main": "ecd21bea0618d42bc536bf1c0d700041",
+".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
+".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
+".git/hooks/pre-commit.sample": "305eadbbcd6f6d2567e033ad12aabbc4",
+".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
+".git/hooks/fsmonitor-watchman.sample": "ea587b0fae70333bce92257152996e70",
+".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
+".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
+".git/hooks/post-update.sample": "2b7ea5cee3c49ff53d41e00785eb974c",
+".git/hooks/pre-merge-commit.sample": "39cb268e2a85d436b9eb6f47614c3cbc",
+".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
+".git/hooks/pre-push.sample": "2c642152299a94e05ea26eae11993b13",
+".git/hooks/update.sample": "647ae13c682f7827c22f5fc08a03674e",
+".git/refs/heads/main": "e3e4f47e4a40fff2c0ded7fc09060601",
+".git/refs/remotes/origin/main": "e3e4f47e4a40fff2c0ded7fc09060601",
+".git/index": "c2cbf51fc05a222f197e7301dae12755",
+".git/COMMIT_EDITMSG": "d23762d6a59655795b47189465a736e8",
+".git/FETCH_HEAD": "4ed6c9079a4a12cdad77a1dbc5f386fa",
+"assets/AssetManifest.json": "84b96ac5adfe85a0d86a50655f718e33",
+"assets/NOTICES": "782309ed1fb436d9e84d5b55ac9ed0c4",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/audio/crunch.mp3": "801314fa35a220068a4081c09fcdbfc7",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
+=======
   ".git/COMMIT_EDITMSG": "d23762d6a59655795b47189465a736e8",
 ".git/config": "6623f6fe716304c8b7a0fd3e635f97ea",
 ".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
@@ -50,6 +103,7 @@ const RESOURCES = {
 "main.dart.js": "b5d88bb1dd68e13e6684e885b259c4d6",
 "manifest.json": "92b981e5821151c62f5e75fdb666ae5f",
 "version.json": "39f479bd9a84277838ad58e5ecb4fbaf"
+>>>>>>> 4172c8766895df6f8837db397290478c3cf86f1b
 };
 
 // The application shell files that are downloaded before a service worker can
